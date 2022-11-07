72d67359f2534be22e580825121f4dec58405b74
Andreas Johannesson <andreas081203@gmail.com>
2022-11-07 20:22:12 +0100

    A connection is now a class and each connection is added to a dict of connections

--- TCP_server.js
+++ TCP_server.js
@@ -4,21 +4,40 @@ let return_message = {
     "message": "hi",
 };
 
+conns = {};
+
+id = 0;
+
+class Connection {
+    constructor(conn, id) {
+        this.conn = conn
+        this.id = id;
+
+        this.conn.on('data', data => {
+            console.log("Data recieved from client " + this.id + ": ", data.toString());
+    
+            this.conn.write(JSON.stringify(return_message) + "\n");
+    
+            console.log("Sending back: ", JSON.stringify(return_message) + "\n");
+    
+        });
+    
+        this.conn.on('end', () => {
+            console.log("Client " + this.id + " left");
+        })
+
+    }
+}
+
 const server = net.createServer(conn => {
     console.log("New client");
 
-    conn.on('data', data => {
-        console.log("Data recieved: ", data.toString());
+    let c = new Connection(conn, id)
 
-        conn.write(JSON.stringify(return_message) + "\n");
+    conns[id] = c;
 
-        console.log("Sending back: ", JSON.stringify(return_message) + "\n");
+    id += 1;
 
-    });
-
-    conn.on('end', () => {
-        console.log("client left");
-    })
 });
 
 server.listen(9000);
