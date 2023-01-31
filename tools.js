604bc4031f20477fe2661b009e884cd9f828b0f1
Andreas Johannesson <andreas081203@gmail.com>
2023-01-31 20:35:36 +0100

    The program can now distinguish between previously known devices and new ones

--- tools.js
+++ tools.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql2');
+const fs = require('fs');
+const { response } = require('express');
 require('dotenv').config();
 
 
@@ -9,65 +11,29 @@ let return_message = {
 };
 
 let deviceTypes = {
-    "SWITCH": [
+    "switch": [
         ["deviceType", "string"],
-        ["deviceName", "string"],
         ["deviceId", "number"],
+        ["userId", "number"],
+        ["deviceName", "string"],
         ["state", "number"]
     ]
 }
 
-let responses = {
-    "initialConnectionComplete": {
-        "status": "SUCCES",
-        "message": "The device has know been registred as known"
-    },
-    "updateOfDataComplete": {
-        "status": "SUCCES",
-        "message": "The data was updated succesfully"
-    },
-    "unReadableData": {
-        "status": "ERROR",
-        "message": "The data could not be parsed as JSON"
-    },
-    "missingFunctionOfData": {
-        "status": "ERROR",
-        "message": "The function of the data could not be understood"
-    },
-    "missingDeviceType": {
-        "status": "ERROR",
-        "message": "The device type was not included"
-    },
-    "missingDeviceId": {
-        "status": "ERROR",
-        "message": "The device ID was not included"
-    },
-    "deviceUnknown": {
-        "status": "ERROR",
-        "message": "Device unknown"
-    },
-    "missingProperties": {
-        "status": "ERROR",
-        "message": "No device properties could be found"
-    },
-    "incorrectProperties": {
-        "status": "ERROR",
-        "message": "Some properties where either missing or was not of the right type"
-    },
-    "incorrectData": {
-        "status": "ERROR",
-        "message": "The data was incorrect"
-    }
-}
+// readFileSync stop the program from continuing before the data has been read
+let rawResponses = fs.readFileSync('responses.json')
 
+let responses = JSON.parse(rawResponses);
 
 // Client object to handle the connection with the client
-function Client(conn, id) {
+function Client(conn, DBconn, onIdentified, onInitialyzation) {
 
     this.conn = conn;
-    this.id = id;
+    this.id = undefined;
 
-    this.status = "unknown";
+    this.identified = false;
+
+    this.properties;
 
     this.evaluateRecievedData = function(data, allPMandatory) {
 
@@ -76,16 +42,16 @@ function Client(conn, id) {
         let correct = true;
         let insertedData = [];
 
-        console.log("Required properties - ", requiredProperties)
-        console.log("Device properties - ", data["properties"])
+        // console.log("Required properties - ", requiredProperties)
+        // console.log("Device properties - ", data["properties"])
         
         for (let i = 0; i < requiredProperties.length; i++) {
 
             // If the propertie was not included in the update and all properties are mandatory the data is incorrect
-            if (! data["properties"][requiredProperties[i][0]] && allPMandatory) correct = false;
+            if (data["properties"][requiredProperties[i][0]] == undefined && allPMandatory) correct = false;
             
-            console.log("typeof ", typeof data["properties"][requiredProperties[i][0]])
-            console.log("required type ", requiredProperties[i][1])
+            // console.log("typeof ", typeof data["properties"][requiredProperties[i][0]])
+            // console.log("required type ", requiredProperties[i][1])
             
             // If the property is not of the correct type and the property is not undefined 
             // (if it was undefined the error would have been caught above)
@@ -100,7 +66,7 @@ function Client(conn, id) {
             }
         }
 
-        if (! correct) return "incorrect"
+        if (correct == false) return "incorrect"
 
         return insertedData;
 
@@ -140,39 +106,79 @@ function Client(conn, id) {
         if (! (data["properties"]["deviceId"])) return this.conn.write(JSON.stringify(responses["missingDeviceId"]) + "\n")
 
 
-        if (this.status == "unknown") {
+        if (this.identified == false) {
+
+            // LOG
+            console.log("Device was not identified");
 
             // If the device is unknown but the function from the client wasnt specified as
             // initialConnection an error is returned
+            if (! (data["function"] == "initialConnection")) console.log("Device did not supply a function of the request");
             if (! (data["function"] == "initialConnection")) return this.conn.write(JSON.stringify(responses["deviceUnknown"]) + "\n");
 
             // If no properties are found an error is also returned
+            if (! data["properties"]) console.log("Device did not supply any properties")
             if (! data["properties"]) return this.conn.write(JSON.stringify(responses["missingProperties"]) + "\n");
 
-            insertedData = this.evaluateRecievedData(data, true)
+            readFromTable(DBconn, data["properties"]["deviceType"], [["deviceId", data["properties"]["deviceId"]]], (result) => {
+                
+                // console.log("Result of database search: " + result)
+                
+                if (result.length > 0) {
+                    
+                    console.log("Device was found in database")
 
-            console.log("INSERTED DATA ", insertedData);
+                    this.id = result[0]["deviceId"];
+                    this.properties = data["properties"];
 
-            if (insertedData == "incorrect") {
+                    this.identified = true;
 
-                // If the program makes it to this point the properties are ready to be inserted into the database
-                // Table must be created first
-                insertIntoTable(conn, table, insertedData)
+                    // CONSIDER THIS
+                    // this.conn.write(JSON.stringify(responses["connectionComplete"]) + "\n");
 
-                // Setting the response back the client
-                response = JSON.stringify(responses["incorrectProperties"]) + "\n";
-            }
-            
-            else {
-                response = JSON.stringify(responses["initialConnectionComplete"]) + "\n"
-                this.status = "known"
+                    //response = JSON.stringify(responses["connectionComplete"]) + "\n";
+                    this.conn.write(JSON.stringify(responses["connectionComplete"]) + "\n");
 
-            }
+                    onIdentified(this.id);
 
-            console.log("INSERTED DATA");
-            console.log("TABLE: ", table)
-            console.log("DATA: ", insertedData)
+                } else {
 
+                    console.log("Device was not found in database")
+
+                    dataToBeInserted = this.evaluateRecievedData(data, true)
+
+                    if (dataToBeInserted == "incorrect") {
+
+                        console.log("Data to be inserted was incorrect")
+
+                        // Setting the response back the client
+                        // response = JSON.stringify(responses["incorrectProperties"]) + "\n";
+                        this.conn.write(JSON.stringify(responses["incorrectProperties"]) + "\n");
+
+                    } else {
+
+                        console.log("Data to be inserted was correct");
+
+                        // If the program makes it to this point the properties are ready to be inserted into the database
+                        // Table must be created first
+                        insertIntoTable(DBconn, table, dataToBeInserted)
+
+                        //response = JSON.stringify(responses["initialConnectionComplete"]) + "\n"
+                        this.conn.write(JSON.stringify(responses["initialConnectionComplete"]) + "\n");
+                        
+                        this.identified = true
+                        this.properties = data["properties"]
+
+
+                        // console.log("INSERTED DATA");
+                        // console.log("TABLE: ", table)
+                        // console.log("DATA: ", dataToBeInserted)
+
+                        onInitialyzation();
+
+                    }
+                }
+            })
         }
 
         if (data["function"] == "updateOfData") {
@@ -185,27 +191,28 @@ function Client(conn, id) {
             updatedData = this.evaluateRecievedData(data, false)
 
             if (updateTable == "incorrect") {
-                response = JSON.stringify(responses["incorrectProperties"]) + "\n";
+                //response = JSON.stringify(responses["incorrectProperties"]) + "\n";
+                this.conn.write(JSON.stringify(responses["incorrectProperties"]) + "\n");
             } else {
-                response = JSON.stringify(responses["updateOfDataComplete"]) + "\n";
+                //response = JSON.stringify(responses["updateOfDataComplete"]) + "\n";
+                this.conn.write(JSON.stringify(responses["incorrectProperties"]) + "\n");
             }
 
-            // At the moment the table is non existing
-            // updateTable(conn,table,where, updatedData);
+            updateTable(DBconn, table, where, updatedData);
 
-            console.log("UPDATED DATA")
-            console.log("WHERE: ", where)
-            console.log("TABLE: ", table)
-            console.log("DATA: ", updatedData)
+            // console.log("UPDATED DATA")
+            // console.log("WHERE: ", where)
+            // console.log("TABLE: ", table)
+            // console.log("DATA: ", updatedData)
 
         }
 
         // Response to client after having succesfully recieved data
         // If response = undefined send back error !
-        if (response == undefined) response = JSON.stringify(responses["incorrectData"]) + "\n";
-        this.conn.write(response);
+        //if (response == undefined) response = JSON.stringify(responses["incorrectData"]) + "\n";
+        //this.conn.write(response);
 
-        console.log("Send back: ", response);
+        //console.log("Send back: ", response);
 
     });
 
@@ -213,7 +220,40 @@ function Client(conn, id) {
 
         console.log("Client " + this.id + ": left");
 
-    })
+    });
+
+    this.propertyChange = function(changes) {
+        // this function should read the database see what has changed and then update the database
+
+        console.log("Device " + this.id + ": property change")
+                
+        list_of_properties = deviceTypes[this.properties["deviceType"]]
+
+        // console.log("property change list of properties ", list_of_properties)
+
+        let list_of_data = [];
+
+        for (let i = 0; i < list_of_properties.length; i++) {
+
+            if (changes[list_of_properties[i][0]]) {
+
+               this.properties[list_of_properties[i][0]] = changes[list_of_properties[i][0]]
+
+            }
+
+            // console.log("pushed ", [list_of_properties[i][0], this.properties[list_of_properties[i][0]]])
+            list_of_data.push([list_of_properties[i][0], this.properties[list_of_properties[i][0]]])
+
+        }
+
+        // console.log("List of data ", list_of_data);
+        
+
+        updateTable(DBconn, this.properties["deviceType"], [["deviceId", this.properties["deviceId"]]], list_of_data);
+
+        
+    };
+
 }
 
 // DATABASE PART ---------------------------------------------------------
@@ -223,17 +263,17 @@ let connectToSqlServer = function() {
     let conn = mysql.createConnection({
         host: process.env.SQL_HOST,
         user: process.env.SQL_USER,
-        password: process.env.PASSWORD,
+        password: process.env.SQL_PASSWORD,
     })
 
     return conn;
 };
 
 // Uses the conn object to select the wanted database
-let setDB = function(db_name) {
+let setDB = function(conn, db_name) {
     conn.query('USE ' + db_name + ";", (err, result) => {
         if (err) throw err;
-        console.log(result);
+        // console.log(result);
     });
 }
 
@@ -258,7 +298,7 @@ let createTable = function(conn, tableName, tableData) {
 
     let sql = "CREATE TABLE " + name + " (" + cols + ");";
    
-    console.log(sql);
+    // console.log(sql);
 
     conn.query(sql, (err, result) => {
         if (err) throw err;
@@ -286,10 +326,11 @@ let insertIntoTable = function(conn, table, data) {
 
     sql = "INSERT INTO " + table + " (" + order + ") VALUES (" + values + ");"
 
-    console.log(sql)
+    // console.log(sql)
 
     conn.query("INSERT INTO " + table + " (" + order + ") VALUES (" + values + ");", (err, result) => {
         if (err) throw err;
+        console.log("Device inserted")
         return result
     })
 }
@@ -306,17 +347,25 @@ let updateTable = function (conn, table, where, data) {
     
     for (let i = 0; i < iterations; i ++) {
 
-        if (where[i]) conditions += where[i][0] + "=" + "'" + where[i][1] + "'";
-        if (data[i]) changes += data[i][0] + "=" + "'" + data[i][1] + "'"; 
+        if (where[i]) {
+            conditions += where[i][0] + "=" + "'" + where[i][1] + "'";
+            // Add commas if not the last
+            if (i != where.length - 1) conditions += ",";
 
+        }
 
-        // Add commas if not the last
-        if (i != data.length - 1) changes += ",";
-        if (i != where.length - 1) conditions += ",";
+        if (data[i]) {
+            changes += data[i][0] + "=" + "'" + data[i][1] + "'"; 
+            // Add commas if not the last
+            if (i != data.length - 1) changes += ",";
+        }
 
+        
     }
 
     let sql = "UPDATE " + table + " SET " + changes + " WHERE " + conditions + ";"
+
+    // console.log(sql)
 
     conn.query(sql, (err, result) => {
         if (err) throw err;
